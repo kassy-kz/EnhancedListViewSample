4492d0738f7430c648dc4cb2edc1a9c674252414
kashimoto <kashimoto.kazutoshi@gmail.com>
2015-02-13 20:34:55 +0900

    最小限のスワイプ消去とundo実装

--- app/src/main/java/orz/kassy/enhancedlistviewtest/MainActivity.java
+++ app/src/main/java/orz/kassy/enhancedlistviewtest/MainActivity.java
@@ -4,11 +4,19 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
+
+import de.timroes.android.listview.EnhancedListView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,7 +27,7 @@ public class MainActivity extends ActionBarActivity {
     };
 
     // リストビュー
-    private ListView mListView;
+    private EnhancedListView mListView;
 
     // リストビューに設定するリストとアダプター
     private LinkedList<String> mItemList;
@@ -31,13 +39,42 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         // リストビュー
-        mListView = (ListView) findViewById(R.id.listview1);
+        mListView = (EnhancedListView) findViewById(R.id.listview1);
 
         // リストビューにアイテム追加
         mItemList = new LinkedList<String>();
         mItemList.addAll(Arrays.asList(INITIAL_LIST));
-        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mItemList);
+        // mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mItemList);
+        mAdapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.text, mItemList);
         mListView.setAdapter(mAdapter);
 
+        // スワイプで消す設定
+        mListView.setDismissCallback(new de.timroes.android.listview.EnhancedListView.OnDismissCallback() {
+            /**
+             * This method will be called when the user swiped a way or deleted it via
+             * {@link de.timroes.android.listview.EnhancedListView#delete(int)}.
+             *
+             * @param listView The {@link EnhancedListView} the item has been deleted from.
+             * @param position The position of the item to delete from your adapter.
+             * @return An {@link de.timroes.android.listview.EnhancedListView.Undoable}, if you want
+             *      to give the user the possibility to undo the deletion.
+             */
+            @Override
+            public EnhancedListView.Undoable onDismiss(EnhancedListView listView, final int position) {
+
+                final String item = (String) mAdapter.getItem(position);
+                /// 消す処理
+                mItemList.remove(position);
+                mAdapter.notifyDataSetChanged();
+                return new EnhancedListView.Undoable() {
+                    @Override
+                    public void undo() {
+                        mItemList.add(position, item);
+                        mAdapter.notifyDataSetChanged();
+                    }
+                };
+            }
+        });
+        mListView.enableSwipeToDismiss();
     }
 }
